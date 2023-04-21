4b65b654da11cc694e97300894c13e8cc88b2d2a
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-20 19:07:58 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /** 
- * main - entry point
- *return: always 0 (success)
+ * main - Entry point
+ *Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
