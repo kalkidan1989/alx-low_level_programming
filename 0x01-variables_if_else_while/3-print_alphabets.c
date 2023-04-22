b0ff20495257d7672f8b4ff310a92dbd14455ab8
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-22 16:23:11 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 for all values
+ */
+int main(void)
+{
+	char	letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+		putchar('\n');
+	return (0);
+}
