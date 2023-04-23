d67938ed829967096c7bec886eaf1925d9ae112c
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-22 17:56:01 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 for all values
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
