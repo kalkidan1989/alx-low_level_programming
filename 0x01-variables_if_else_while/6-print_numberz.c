136da84605e95a34165c6740b64ae05986118b2b
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-22 17:42:13 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 for all values
+ */
+int main(void)
+{
+	int     num;
+
+	for     (num =       0;    num  <=      9;    num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
