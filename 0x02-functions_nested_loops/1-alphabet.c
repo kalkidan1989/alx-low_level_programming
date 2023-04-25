16630cec8a59f3d0d8206f9d4333d48fb1e35952
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-25 13:00:42 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all lowercase letters
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for	(i	=	'a';	i	<=	'z';	i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
