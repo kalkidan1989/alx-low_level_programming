c3646aa3fda1847bb0b5d6774c4cc085031e9219
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-28 13:17:37 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -  checks for a digit
+ *@a:   the     number  to      be      multiplied
+ *@b:	the	number	to	be	multiplied
+ * Return: Always a	*	b
+ */
+int mul(int a, int b)
+{
+	return	(a	*	b);
+}
