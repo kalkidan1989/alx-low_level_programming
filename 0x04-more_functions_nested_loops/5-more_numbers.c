0b12072c2ea392a3a33ece0ff3075cfbc50cbe2b
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-04-28 14:34:24 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *      more_numbers -  print numbers
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int     i;
+	int	j;
+
+	for     (i      =       1;     i       <=	10;     i++)
+	{
+		for     (j      =       0;     j       <=       14;     j++)
+		{
+			if	(j	>	9)
+			{
+				_putchar((j	/	10)	+	48);
+			}
+			_putchar((j	%	10)	+	48);
+		}
+	}
+	_putchar('\n');
+}
