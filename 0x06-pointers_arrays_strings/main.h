046e8f70bdadf9109043e524fc70e114e48de6ab
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-05-04 15:50:06 -0700

    Task 1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);                     
 char *_strcat(char *dest, char *src);
-char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
+char *_strncat(char *dest, char *src, int n);                                                                              
 char *_strchr(char *s, char c);                                                                                                    
 unsigned int _strspn(char *s, char *accept);                                                                                       
 char *_strpbrk(char *s, char *accept);                                                                                             
