747ec75f46dfa329ad155ad2f12b9fc634d6f025
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-05-04 16:49:32 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *start = dest;
+	int i;
 
-	if ((dest == NULL) &&	(src == NULL))
-		return (NULL);
-	while (*src && n--)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (start);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
