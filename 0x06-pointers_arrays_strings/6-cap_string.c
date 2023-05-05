2977067cf240fb4b97a0fb8e143f026945a8862e
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-05-04 21:02:25 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,35 +5,30 @@
 
 /**
  * cap_string - a function that concatenates two strings
- *@a:        input  character
+ *@s:        input  character
  * Return: a pointer to the resulting string
  */
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
-	int     i;
+	int count = 0;
+	int	i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for	(i	=	0;	a[i]	!=	'\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if	(i	==	0)
+		for (i = 0; i < 13; i++)
 		{
-			if	((a[i]	>=	'a') && (a[i]	<=	'z'))
-				a[i]	=	a[i]	-	32;
-			continue;
-		}
-		if	(a[i]	==	' '	||	a[i]   ==      '\t'	||	a[i]   ==      '\n'	||	(a[i]   ==      33	&&	a[i]   ==      ' ')	||	((a[i]   ==      46	&&	a[i]   ==      ' ')))
-		{
-			++i;
-			if	(a[i]	>=	'a' && a[i]	<=	'z')
+			if (*(s + count) == sep_words[i])
 			{
-				a[i]	=	a[i]	-	32;
-				continue;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		else
-		{
-			if	(a[i]	>=	'A' && a[i]	<=	'Z')
-				a[i]	=	a[i]	+	32;
-		}
+		count++;
 	}
-	return  (a);
+	return (s);
 }
