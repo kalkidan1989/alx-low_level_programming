16bb9444ad5bbf0450efe9c82f07ed7b04155d01
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-08-03 15:42:35 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int kal, quoter = 0;
+	unsigned long int ahune	=	0;
+	unsigned long int wotet = n ^ m;
+
+	for (kal = 63; kal >= 0; kal--)
+	{
+		quoter = wotet >> kal;
+		if (ahune & 1)
+			quoter++;
+	}
+
+	return (quoter);
+}
