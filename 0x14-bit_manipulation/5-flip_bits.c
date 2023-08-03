79f7e2ef904a91673517a3fafc05297aab8a0683
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-08-03 15:50:16 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int kal, quoter = 0;
-	unsigned long int ahune	=	0;
+	unsigned long int ahune	=	0UL;
 	unsigned long int wotet = n ^ m;
 
 	for (kal = 63; kal >= 0; kal--)
