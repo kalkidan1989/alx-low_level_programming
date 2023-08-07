bbb3707c24ed0e8d5ab11b08ef9adaaaf4cd1944
kalkidan1989 <kalkidanbasazinew.d@gmail.com>
2023-08-07 16:10:59 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int x, y, z = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (z = 0; text_content[z];)
+			z++;
+	}
+
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(x, text_content, z);
+
+	if (x == -1 || y == -1)
+		return (-1);
+
+	close(x);
+
+	return (1);
+}
+
